ae5ee1db0407a94017e261c48dc7c2d152aa5e32
itspasindu <117925170+itspasindu@users.noreply.github.com>
2022-12-01 10:23:35 +0530

    upload files

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC5ZoBguJD_Dr2QC8u-iSjlZv2qdXOvMJI",
+    authDomain: "sign-up-d65f2.firebaseapp.com",
+    projectId: "sign-up-d65f2",
+    storageBucket: "sign-up-d65f2.appspot.com",
+    messagingSenderId: "748427381019",
+    appId: "1:748427381019:web:29945daaa73a07cd7b6bbe"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
---
+++ index.js
@@ -0,0 +1,58 @@
+document.getElementById("loginForm").addEventListener("submit",(event)=>{
+    event.preventDefault()
+})
+
+firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+        location.replace("welcome.html")
+    }
+})
+
+function login(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch((error)=>{
+        document.getElementById("error").innerHTML = error.message
+    })
+}
+
+function signUp(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+
+    .catch((error) => {
+        document.getElementById("error").innerHTML = error.message
+    });
+}
+
+function forgotPass(){
+    const email = document.getElementById("email").value
+    firebase.auth().sendPasswordResetEmail(email)
+    .then(() => {
+        alert("Reset link sent to your email id")
+    })
+    .catch((error) => {
+        document.getElementById("error").innerHTML = error.message
+    });
+}
+
+
+const signInWithFacebook = () => {
+  const facebookProvider = new firebase.auth.FacebookAuthProvider();
+
+  auth.signInWithPopup(facebookProvider)
+  .then(() => {
+    window.location.assign('./profile');
+  })
+  .catch(error => {
+    console.error(error);
+  })
+}
+
+signInWithFacebookButton.addEventListener('click', signInWithFacebook);
+
+
+
+
